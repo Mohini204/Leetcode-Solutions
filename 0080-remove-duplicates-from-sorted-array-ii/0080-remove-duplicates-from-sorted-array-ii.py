0cbc9934d93435a60d4d9e7c52adc583aa857770
Mohini204 <60132762+Mohini204@users.noreply.github.com>
2023-10-23 14:46:46 -0400

    Time: 55 ms (75.31%), Space: 16.4 MB (31.10%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -5,17 +5,22 @@ class Solution:
         
         if len(nums) == 1: return 1
         
-        c,i = 1,1
+        c,i, j = 1,1,1
         while i < len(nums):
             if nums[i] == nums[i-1]:
                 c+=1
-                if c > 2:
-                    nums.pop(i)
-                    i-=1
-                i+=1
+                if c <= 2:
+                    nums[j] = nums[i]
+                    i+=1
+                    j+=1
+                else:
+                    i+=1
+                    
             else:
+                c = 1
+                nums[j] = nums[i]
                 i+=1
-                c=1
-            
-        return len(nums)
+                j+=1
+                
+        return j
         
